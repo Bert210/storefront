ab00e27e9c81700fcd5ee795715f4894cdc09e71
Robert <bert210@gmail.com>
2017-10-29 00:14:24 -0400

    Added title bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import glamorous from 'glamorous'
+
+const Info = glamorous.div({
+  height: 20,
+  padding: 5,
+  backgroundColor: 'black',
+})
+
+const TitleContainer = glamorous.div({
+  // height: 80,
+  width: '100%',
+  padding: '0 2rem',
+  display: 'flex',
+  boxShadow: '0 4px 4px #eee',
+})
+
+const Title = glamorous.span({
+  fontSize: 36,
+  padding: '0.5rem 0',
+  flex: 1,
+})
+
+const Links = glamorous.span({
+  // justifyContent: 'flex-end',
+  display: 'flex',
+  flex: 4,
+  justifyContent: 'flex-end',
+  alignSelf: 'center',
+  // padding: '0.5rem 0',
+})
+
+const Link = glamorous.span({
+  // height: '100%',
+  // flexDirection: 'row',
+  padding: '0 0.5rem',
+  // alignItems: 'center',
+})
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Info />
+        <TitleContainer>
+          <Title>Store</Title>
+          <Links>
+            <Link>Brands</Link>
+            <Link>Departments</Link>
+            <Link>Journal</Link>
+            <Link>Contact</Link>
+            <Link>CART</Link>
+          </Links>
+        </TitleContainer>
       </div>
     );
   }
